67911090983b47a31fc95c5254796c4cf495bd5f
mmelucci <mmelucci20@stuy.edu>
2021-01-12 21:45:29 -0500

    Completed merge method

--- Radix.java
+++ Radix.java
@@ -5,18 +5,22 @@ public class Radix{
     return Math.abs((n / (int)Math.pow(10, col) % 10));
   }
 
+// return the number of digits in n.
   public static int length(int n) {
     if (n == 0) {
       return 1;
     }
     return ((int) (Math.log10(Math.abs(n)) + 1));
   }
-// return the number of digits in n.
 
-// Second Part:
-//
-// 3. public static void merge(MyLinkedList original,MyLinkedList[]buckets)
-// Merge all of the linked lists in the bucket array into your original linked list. The original may have elements, and those should be kept.
+  public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
+    for (int i = 0; i < buckets.length; i ++) {
+      original.extend(buckets[i]);
+    }
+}
+
+// Merge all of the linked lists in the bucket array into your original linked list.
+// The original may have elements, and those should be kept.
 // This is O(buckets.length) which should be 10 when we use this later.
 // The bucket[0] list will be merged first, then the bucket[1] etc.
 // Write and thoroughly test these methods!
